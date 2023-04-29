ce1f6cab34f4786bd48ce7f1326af566c578091d
Florin <florinconstantin99@yahoo.com>
2023-04-29 10:54:02 +0300

    testing functionality

--- pages/About.tsx
+++ pages/About.tsx
@@ -5,6 +5,8 @@ function About() {
     <div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, modi.
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, tempora.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam,
+      fugit.
     </div>
   );
 }
